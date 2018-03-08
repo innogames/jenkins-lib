39e760dd8a380e8e01f2cbf0fb223a9eaf6ae70e
Mikhail Shiryaev <mikhail.shiryaev@innogames.com>
2018-03-08 10:04:31 +0100

    Work with gitSteps is almost done

--- vars/gitSteps.groovy
+++ vars/gitSteps.groovy
@@ -9,17 +9,34 @@ def Version(String upd, Map part) {
             return "${part.major}.${part.minor + 1}.0"
         case 'major':
             return "${part.major + 1}.0.0"
+        default:
+            error('Something wrong in parameters')
     }
+}
+def BranchOrTag(env) {
+    if ([env['TAG_DATE'], env['TAG_NAME'], env['TAG_TIMESTAMP'], env['TAG_UNIXTIME']] != [null, null, null, null]) {
+        return 'tag'
+    } else if (env['BRANCH_NAME'] != null) {
+        return 'branch'
+    }
+    return null
 }
 
 def call(Map config) {
+    // !!! There impossible to invoke env.VAR in modules, only env['VAR']
     // Getting all parameters in env
     params.each{ k, v ->
         env[k] = v
     }
     // Every UPPERCASE var should be passed into env
-    // Checking out sources
-    def results = checkout scm
+    // Checking out sources with tags
+    def results = checkout([
+        $class: 'GitSCM',
+        branches: scm.branches,
+        doGenerateSubmoduleConfigurations: scm.doGenerateSubmoduleConfigurations,
+        extensions: [[$class: 'CloneOption', noTags: false, shallow: false, depth: 0, reference: '']],
+        userRemoteConfigs: scm.userRemoteConfigs,
+    ])
 
     // Map all results variables into env
     results.each{ k, v ->
@@ -27,40 +44,32 @@ def call(Map config) {
     }
 
     // Remote repo name
-    env.GIT_REMOTE = sh(returnStdout: true, script: 'git remote').trim()
+    env['GIT_REMOTE'] = sh(returnStdout: true, script: 'git remote').trim()
 
     // Do we building from branch or tag?
-    env.GIT_BRANCH_OR_TAG = sh(returnStdout: true, script:
-    """\
-        #!/bin/bash -e
-        if git rev-parse --verify -q refs/remotes/${env.GIT_REMOTE}/${env.GIT_BRANCH}^{} | grep -q ${env.GIT_COMMIT}
-          then echo branch
-          exit 0
-        elif git rev-parse --verify -q refs/tags/${env.GIT_BRANCH}^{} | grep -q ${env.GIT_COMMIT}
-          then echo tag
-          exit 0
-        else
-          echo unknown
-          exit 0
-      fi
-    """.stripIndent()).trim()
-
+    echo "${env.getClass()}"
+    env['GIT_BRANCH_OR_TAG'] = BranchOrTag(env)
     // Last version from tags
-    env.GIT_LAST_VERSION = sh(returnStdout: true,
+    env['GIT_LAST_VERSION'] = sh(returnStdout: true,
         script: 'git tag -l "v[0-9]*"| sort -V | tail -n 1').trim()
 
     // Geting version parts
-    def Map versionParts = [:]
-    if (!(env.GIT_LAST_VERSION =~ /^v(\d+\.)+\d+$/)) {
-        echo "There no or wrong last version in tags (${env.GIT_LAST_VERSION}), creating the first release 0.0.1"
-        versionParts.with{ (major, minor, patch) = [0, 0, 1] }
+    versionParts = [:]
+    if (!(env['GIT_LAST_VERSION'] =~ /^v(\d+\.)+\d+$/)) {
+        echo "There no or wrong last version in tags (${env['GIT_LAST_VERSION']}), creating the first release 0.0.1"
+        versionParts.major = 0
+        versionParts.minor = 0
+        versionParts.patch = 1
     } else {
-        versionParts.with{ (major, minor, patch) = env.GIT_LAST_VERSION.replaceFirst(~/^v/,'').tokenize('.') }
-        versionParts.each{ k, v ->
-            versionParts[k] = v?.isInteger() ? v.toInteger() : 0
+        vp = env['GIT_LAST_VERSION'].replaceFirst(~/^v/,'').tokenize('.')
+        vp.eachWithIndex{v, k ->
+            vp[k] = v?.isInteger() ? v.toInteger() : 0
         }
+        versionParts.major = vp[0]
+        versionParts.minor = vp[1]
+        versionParts.patch = vp[2]
     }
-    env.NEW_VERSION = Version(params.UPDATE_VERSION, versionParts)
+    env['NEW_VERSION'] = Version(params.UPDATE_VERSION, versionParts)
 
-    env.GIT_NEW_TAG = "v${env.VERSION_FOR_TAGING}"
+    env['GIT_NEW_TAG'] = "v${env['NEW_VERSION']}"
 }
